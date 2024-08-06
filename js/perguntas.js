ab34384f561a56aae591f1055f7d97eaa4bae502
gabrielefilipak <130515783+gabrielefilipak@users.noreply.github.com>
2024-08-06 10:25:49 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,104 @@
+export const perguntas = [
+    {
+        enunciado: " O vestibular é de extrema importância para garantir um futuro instável e próspero, afinal ele é a forma de ingressar em uma faculdade, mas para que isso aconteça é necessário uma longa jornada de estudos, que demanda muito foco e atenção. E você já pensou em prestar vestibular?",
+        alternativas: [
+            {
+                texto:  "Sim, quero garantir o meu futuro",
+                afirmacao: [
+             "Ao prestar o vestibular você pode garantir um futuro brilhante. Além de garantir conhecimento na área/profissão escolhida, estudos comprovam que pessoas com ensino superior possuem uma taxa menor de desemprego.",
+             "O vestibular é uma forma de ingressar nas faculdades/universidades e assim garantir um futuro instável e por vezes brilhante."
+             ]
+            },   
+            {
+                texto: "Não me importo com isso",
+                afirmacao: [
+                " Ao não prestar vestibular você poderá estar desperdiçando seu futuro, por isso pense bem na sua decisão!",
+                "Ao não prestar o vestibular, possivelmente você terá o seu futuro afetado, pois as ações realizadas no presente tem significativa importância no futuro."
+              ]
+            }
+           
+        ]
+    },
+    {
+        enunciado: "Você já sabe o que quer do seu futuro?",
+        alternativas: [
+            {
+                texto: "Sim, estou completamente certo(a)!",
+                afirmacao: [
+                    "Isso indica que você planeja seu futuro e se continuar assim certamente irá trilhar uma carreira de sucesso.",
+                    "Saber o que deseja para o futuro é fundamental para conquistar o que você planeja."
+            ]
+            },
+            {
+                texto: "Ainda não sei direito",
+                afirmacao:[
+                    "Algumas pessoas se sentem inseguras e talvez indecisas com o futuro, mas não se preocupe com o tempo as coisas vão fluir! Mas é sempre bom começar a pensar nas coisas futuras, principlamente em metas e objetivos para se alcançar ao longo da vida.",
+                    "Para dar o primeiro é preciso saer o caminho que deseja seguir, por isso começa a pensar o que deseja no seu futuro."
+            ]
+            }
+           
+        ]
+    },
+    {
+        enunciado: "Já parou para pensar onde você quer estar daqui à alguns anos?",
+        alternativas: [
+            {
+                texto:"Sim, sempre penso no meu futuro!",
+                afirmacao: [
+                    "Se você pense no seu futuro é sinal que você já deu um grande passo, afinal todas as conquistas se iniciam com um sonho e com a vontade de fazer ele virar realidade.",
+                    "Pensar nos seus planos é essencial para realizá-los, pois o pensamento pode se tornar uma fonte de inspiração e determinação."
+            ]
+            },
+            {
+                texto:  "Nunca parei para pensar",
+                afirmacao:[
+                    "Nem todas as pessoas conseguem se ver no futuro e isso é muito comum, mas lembre-se que é sonhando que se faz acontecer, e se imaginar exercendo a profissão tão sonhada é o combustível que alimenta esse sonho.",
+                    "Pensar no seu futuro pode se tornar uma fonte de força e determinação para seguir em frente."
+            ]
+            }
+            
+        ]
+    },
+    {
+        enunciado: "Você se esforça para conquistar suas metas?",
+        alternativas: [
+            {
+                texto:"Sim, muito!",
+                afirmacao: [
+                    "Se você continuar se esforçando dessa maneira, certamente vai ir muito longe. Pessoas geniais chegam a lugares incriveis, mas pessoas esforçadas chegam a lugares extraordinários!",
+                    "O suceso é a soma de pequenos esforços dia após dia, por isso não deixe de se esforçar, por que um dia os seus esforços te levaram muito longe "
+             ]
+            },
+            {
+                texto: "Não muito",
+                afirmacao:[
+                    "Para se alcançar uma meta é necessário muito esforço e dedicação, afinal de contas as coisa não caem do céu, muito pelo contrário, elas vêm com muito esforço, por isso trate de se empenhar par alcançar suas metas, porque na vida não há lugar para todos, apenas para os melhores.",
+                "As nossas metas e esforços são essenciais para alcançar os nossos objetivos."
+            ]
+            }
+          
+        ]
+    },
+    {
+        enunciado: "Vocẽ já parou para pensar qual faculdade/universidade quer estudar? ",
+        alternativas: [
+            {
+                texto: "Já tenho algumas em mente",
+                afirmacao: [
+                    "É sempre bom traçar metas, e a escolha da faculdade/universidade é uma decissão muito importante. ",
+                    "As faculdades/universidades são o passaporte para se realizar um sonho,por isso a escolha a faculdade/universidade é uma escolha importante e decisiva."
+            ]
+            },
+            {
+                texto: "Não faço à mínima ideia",
+                afirmacao:[
+                    "Comece procurar e pesquisar a respeito, afinal a escolha da faculadade/universidade é o ínicio de tudo.",
+                    "Todo sonho, assim como todos as coisas nessa vida se iniciam pelo começo, que nesse caso seria a escolha da faculdade/universidade"
+             ]
+            }
+           
+        ]
+    },
+    
+
+];
